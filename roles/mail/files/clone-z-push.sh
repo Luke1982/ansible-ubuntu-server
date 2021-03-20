4e0f6fb95d6d9ca3dad167590e453096ae017d6d
Luke1982 <gcx3@hotmail.com>
2021-03-20 10:55:20 +0100

    Fix Z-push clone script

--- roles/mail/files/clone-z-push.sh
+++ roles/mail/files/clone-z-push.sh
@@ -3,6 +3,6 @@
 DOMAIN=$1
 
 ## Avoid error when trying to clone into an already existing directory
-if [ ! -d "$DOMAIN"] ; then
+if [ ! -d "$DOMAIN" ] ; then
 	git clone --single-branch --branch=master https://github.com/Z-Hub/Z-Push ${DOMAIN}
 fi
