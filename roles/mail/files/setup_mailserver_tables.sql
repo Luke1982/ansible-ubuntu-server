b18bf05f215da28116a42e5ff558b5cdac7778e6
Luke1982 <gcx3@hotmail.com>
2021-04-05 14:07:18 +0200

    Setup sender aliases for postfix

--- roles/mail/files/setup_mailserver_tables.sql
+++ roles/mail/files/setup_mailserver_tables.sql
@@ -22,3 +22,13 @@ CREATE TABLE IF NOT EXISTS `virtual_domains` (
  PRIMARY KEY (`id`),
  FOREIGN KEY (domain_id) REFERENCES virtual_domains(id) ON DELETE CASCADE
  ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `virtual_sender_aliases` (
+ `id` int(11) NOT NULL auto_increment,
+ `domain_id` int(11) NOT NULL,
+ `users` varchar(255) NOT NULL,
+ `alias` varchar(100) NOT NULL,
+ PRIMARY KEY (`id`),
+ CONSTRAINT alias_unique UNIQUE (alias),
+ FOREIGN KEY (domain_id) REFERENCES virtual_domains(id) ON DELETE CASCADE
+ ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
